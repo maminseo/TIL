0509d7be485516645e418cd96b24415008c77509
Minseo Ma <mamin1129@naver.com>
2026-02-06 16:27:56 +0900

    Title: 국영수 순이지, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260206/국영수 순이지/korean-english-math-order.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+class student{
+    public:
+        string name;
+        int kor,eng,math;
+
+        student(string name, int kor, int eng, int math) {
+            this->name =name;
+            this->kor =kor;
+            this->eng =eng;
+            this->math =math;
+        }
+        student(){}
+
+};
+
+bool cmp(student a, student b) {
+    if(a.kor==b.kor) {
+        if(a.eng == b.eng) return a.math > b.math;
+        return a.eng > b.eng;
+    }
+    return a.kor > b.kor;
+}
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    student students[n];
+
+    string name;
+    int kor,eng,math;
+
+    for(int i=0;i<n;i++) {
+        cin >> name >> kor >> eng >> math;
+        students[i] = student(name,kor,eng,math);
+    }
+    sort(students,students+n,cmp);
+
+    for(int i=0;i<n;i++) {
+        cout << students[i].name << " " << students[i].kor << " " << students[i].eng << " " << students[i].math << endl;
+    }
+
+    return 0;
+}
